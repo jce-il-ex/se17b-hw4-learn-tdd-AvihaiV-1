5ba53563831b1e5b08c8682790482c84672b6668
AvihaiV <avihai673@gmail.com>
2017-05-31 22:18:15 +0300

    GREEN

--- test/test.js
+++ test/test.js
@@ -1,16 +1,12 @@
 var assert = require("assert"); // node.js core module
 var C = require('../cash.js');  // our module
-C.getChange = function (totalPayable, cashPaid) {
-    'use strict';
-    if(totalPayable == 486 && cashPaid == 1000)
-        return [500, 10, 2, 2];
-    else if(totalPayable == 210 && cashPaid == 300)
-        return [50, 20, 20];
-};
  
 describe('Cash Register', function(){
   describe('Module C', function(){
-    it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+       it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
+    assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
+})
+       it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
     assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
      })
     it('getChange(210,300) should equal [50,20,20]', function(){
